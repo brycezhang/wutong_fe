2409948c1e39e08c03e95e26d918758cc5e236cc
Bryce <brycezhang.cn@gmail.com>
2025-11-19 17:53:07 +0800

    refactor: 文件名和路径

--- components/Footer.js
+++ components/Footer.js
@@ -3,14 +3,14 @@ class SiteFooter extends HTMLElement {
     this.innerHTML = `
       <footer class="site-footer">
         <div class="container footer-container">
-          <a href="/" class="footer-logo">
+          <a href="https://www.wutongshuxia.com.cn/" class="footer-logo">
             <div class="logo-icon"></div>
             <span class="logo-text">梧桐树下</span>
           </a>
           <div class="footer-links">
             <a href="/contact.html">联系我们</a>
-            <a href="/terms.html">用户协议</a>
-            <a href="/privacy.html">隐私政策</a>
+            <a href="/termsofservice.html">用户协议</a>
+            <a href="/privacypolicy.html">隐私政策</a>
           </div>
           <div class="footer-copyright">
             <p>© 2025 千言一语文化传媒有限公司. All rights reserved.</p>
