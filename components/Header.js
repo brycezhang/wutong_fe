b6359ba6fe1ac6a8a12014ac6f303415cada3c26
Bryce <brycezhang.cn@gmail.com>
2025-11-20 18:13:21 +0800

    feat: 优化header的样式

--- components/Header.js
+++ components/Header.js
@@ -8,7 +8,7 @@ class SiteHeader extends HTMLElement {
             <div class="logo">梧桐树下</div>
           </a>
           <nav class="main-nav">
-            <a href="/contact.html">联系我们</a>
+            <a href="/contact.html" id="header-contact">联系我们</a>
             <a href="#" class="btn btn-black js-download-btn">下载</a>
           </nav>
         </div>
